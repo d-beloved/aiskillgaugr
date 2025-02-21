9ad2ff4d7b7b084b6763c5421c19a6b14452d09e
d-beloved <morayodeji@gmail.com>
2025-02-21 18:03:39 +0100

    update the +Head file

--- src/pages/+Head.tsx
+++ src/pages/+Head.tsx
@@ -4,21 +4,20 @@ import React from "react";
 import logoUrl from "../assets/logo.svg";
 
 export default function HeadDefault() {
+  const gaKey = import.meta.env.PUBLIC_ENV__GOOGLE_ANALYTICS;
+
   return (
     <>
       <link rel="icon" href={logoUrl} />
 
-      <script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${import.meta.env.PUBLIC_ENV__GOOGLE_ANALYTICS}`}
-      ></script>
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaKey}`}></script>
       <script
         dangerouslySetInnerHTML={{
           __html: `window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', '${import.meta.env.PUBLIC_ENV__GOOGLE_ANALYTICS}');`,
+          gtag('config', '${gaKey}');`,
         }}
       ></script>
     </>
