2a19f8615a6806f3de5b48e39f70cebc5cb16b14
d-beloved <morayodeji@gmail.com>
2025-02-28 23:09:05 +0100

    update layout and quiz preferences modal

--- src/components/QuizOptionsModal.tsx
+++ src/components/QuizOptionsModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, FC } from "react";
+import React, { useState, FC } from "react";
 import { useData } from "vike-react/useData";
 import { navigate } from "vike/client/router";
 import { Data } from "@/pages/index/+data";
@@ -23,14 +23,6 @@ const QuizOptionsModal: FC<QuizOptionsModalProps> = ({ showModal, onClose }) => 
           quizCount: "",
         },
   );
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const modal = document.getElementById("quizOptionsModal") as HTMLDialogElement;
-    if (modal && showModal) {
-      modal.showModal();
-    }
-  }, [showModal]);
 
   const handlePreferenceChange = (key: string, value: string) => {
     setPreferences((prevPreferences) => ({
@@ -41,15 +33,12 @@ const QuizOptionsModal: FC<QuizOptionsModalProps> = ({ showModal, onClose }) => 
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    setLoading(true);
 
     try {
       await startNewQuiz(preferences);
       navigate("/quiz");
     } catch (error) {
       console.error("Failed to start quiz");
-    } finally {
-      setLoading(false);
     }
   };
 
@@ -57,84 +46,80 @@ const QuizOptionsModal: FC<QuizOptionsModalProps> = ({ showModal, onClose }) => 
   const isReady = language && level && quizCount;
 
   return (
-    <dialog id="quizOptionsModal" className="modal">
-      {/* {isLoading && <div className="loading loading-spinner"></div>} */}
-      <div className="modal-box">
-        <form method="dialog" onSubmit={handleSubmit}>
-          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={onClose}>
-            ✕
-          </button>
-          <h3 className="font-bold text-lg text-center">Select Quiz Options</h3>
-          <div className="py-4">
-            <div className="mb-4">
-              <label htmlFor="language" className="block text-sm font-medium text-gray-700">
-                Select Programming Language:
-              </label>
-              <select
-                id="language"
-                value={language}
-                onChange={(e) => handlePreferenceChange("language", e.target.value)}
-                required
-                className="mt-1 block w-full"
-              >
-                <option value="" disabled>
-                  --Select--
+    <dialog className={`modal ${showModal ? "modal-open" : ""}`}>
+      <div className="modal-box bg-gray-800 text-white">
+        <h3 className="font-bold text-2xl mb-6">Quiz Preferences</h3>
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div className="form-control">
+            <select
+              id="language"
+              value={language}
+              onChange={(e) => handlePreferenceChange("language", e.target.value)}
+              required
+              className="select select-bordered w-full bg-gray-700 focus:outline-none"
+            >
+              <option value="" disabled>
+                Select a Programming Language
+              </option>
+              {languages.map((language) => (
+                <option key={language} value={language}>
+                  {language}
                 </option>
-                {languages.map((language) => (
-                  <option key={language} value={language}>
-                    {language}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="mb-4">
-              <label htmlFor="level" className="block text-sm font-medium text-gray-700">
-                Select Level of Competence:
-              </label>
-              <select
-                id="level"
-                value={level}
-                onChange={(e) => handlePreferenceChange("level", e.target.value)}
-                required
-                className="mt-1 block w-full"
-              >
-                <option value="" disabled>
-                  --Select--
+              ))}
+            </select>
+          </div>
+
+          <div className="form-control">
+            <select
+              id="level"
+              value={level}
+              onChange={(e) => handlePreferenceChange("level", e.target.value)}
+              required
+              className="select select-bordered w-full bg-gray-700 focus:outline-none"
+            >
+              <option value="" disabled>
+                Select a Difficulty Level
+              </option>
+              {levels.map((level) => (
+                <option key={level} value={level}>
+                  {level}
                 </option>
-                {levels.map((level) => (
-                  <option key={level} value={level}>
-                    {level}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="mb-4">
-              <label htmlFor="numberOfQuizzes" className="block text-sm font-medium text-gray-700">
-                Number of Quizzes:
-              </label>
-              <select
-                id="quizCount"
-                value={quizCount}
-                onChange={(e) => handlePreferenceChange("quizCount", e.target.value)}
-                required
-                className="mt-1 block w-full"
-              >
-                <option value="" disabled>
-                  --Select--
+              ))}
+            </select>
+          </div>
+
+          <div className="form-control">
+            <select
+              id="quizCount"
+              value={quizCount}
+              onChange={(e) => handlePreferenceChange("quizCount", e.target.value)}
+              required
+              className="select select-bordered w-full bg-gray-700 focus:outline-none"
+            >
+              <option value="" disabled>
+                Select Number of Questions
+              </option>
+              {quizCounts.map((opt) => (
+                <option key={opt} value={opt}>
+                  {opt} Questions
                 </option>
-                {quizCounts.map((opt) => (
-                  <option key={opt} value={opt}>
-                    {opt}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <button type="submit" className="btn btn-primary" disabled={!isReady || loading}>
-              Start Quiz
+              ))}
+            </select>
+          </div>
+
+          <div className="modal-action">
+            <button type="button" className="btn btn-ghost" onClick={onClose} disabled={isLoading}>
+              Cancel
+            </button>
+            <button type="submit" className="btn btn-primary" disabled={!isReady || isLoading}>
+              {isLoading ? <span className="loading loading-spinner"></span> : "Start Quiz"}
             </button>
           </div>
         </form>
       </div>
+      <form method="dialog" className="modal-backdrop">
+        <button onClick={onClose}>close</button>
+      </form>
     </dialog>
   );
 };
